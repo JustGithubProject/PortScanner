ee20e83c88b72c9a1126cda8831649d8e43fe223
JustGithubProject <Kropivadanil@gmail.com>
2024-11-10 15:22:48 +0200

    Init

---
+++ scanner.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"syscall"
+)
+
+func main() {
+	// Destination host and port
+	host := "scanme.nmap.org"
+	port := 80
+
+	// Create a raw socket (fd -> file descriptor)
+	fd, err := syscall.Socket(
+		syscall.AF_INET,
+		syscall.SOCK_RAW,
+		syscall.IPPROTO_TCP,
+	)
+	
+	if err != nil {
+		log.Println("Failed to create raw socket")
+		os.Exit(1)
+	}
+
+	
+}
