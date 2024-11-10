db5d69dd8d5efd9ed802cac9acd113c381d36646
JustGithubProject <Kropivadanil@gmail.com>
2024-11-10 20:31:28 +0200

    Added checkSum function to check data consistency

--- scanner.go
+++ scanner.go
@@ -8,7 +8,42 @@ import (
 	"syscall"
 )
 
-func createSynPacket(srcIP string, dstIP string, srcPort uint16, dstPort uint16) {
+// The function for data consistency
+func checkSum(data []byte) uint16 {
+	sum := uint32(0)
+	/*
+	EXAMPLE:
+		data[i]=0xFF 
+		and 
+		data[i+1]=0x65
+		sum = sum + 0xFF65
+	*/
+	for i := 0; i < len(data) - 1; i += 2 {
+		sum += uint32(data[i]) << 8 + uint32(data[i + 1])
+	}
+
+	/*
+	EXAMPLE:
+		data[len(data) - 1]=0x88
+		sum = sum + 0x8800
+
+		data[i] = 0000 0000 0000 0000 0000 0000 0000 0001 -> 0x01
+		0000 0000 0000 0000 0000 0001 0000 0000
+		data[i + 1] = 0000 0000 0000 0000 0000 0000 0000 0001 -> 0x01
+		0000 0000 0000 0000 0000 0001 0000 0001
+
+		RESULT: 0x0101
+	*/
+	if len(data) % 2 == 1 {
+		sum += uint32(data[len(data) - 1]) << 8
+	}
+
+	sum = (sum >> 16) + (sum & 0xFFFF)
+	sum += sum >> 16
+	return uint16(^sum)
+}
+
+func createSynPacket(srcIP string, dstIP string, srcPort uint16, dstPort uint16) []byte {
 
 	readyToUseSrcIP := net.ParseIP(srcIP).To4()
 	readyToUseDstIP := net.ParseIP(dstIP).To4()
@@ -16,6 +51,8 @@ func createSynPacket(srcIP string, dstIP string, srcPort uint16, dstPort uint16)
 
 	// Creating buffer for packet (IP-header 20 bytes + TCP-header 20 bytes)
 	packet := make([]byte, 40)
+
+	// IP-Header
 	packet[0] = 0x45  // Version=4 and length=5 * 4 = 20 bytes, it takes 8 bit (0x45 - 1 byte)
 	packet[1] = 0x00  // TOS
 	binary.BigEndian.PutUint16(packet[2:4], 40) // Length of packet (IP + TCP)
@@ -24,12 +61,14 @@ func createSynPacket(srcIP string, dstIP string, srcPort uint16, dstPort uint16)
 	packet[7] = 0x00 // Fragment Offset
 	packet[8] = 64   // TTL
 	packet[9] = syscall.IPPROTO_TCP
-	copy(packet[12:16], readyToUseSrcIP)
-	copy(packet[16:20], readyToUseDstIP)
+	copy(packet[12:16], readyToUseSrcIP) // Source address
+	copy(packet[16:20], readyToUseDstIP) // Destination address
 
-	// TODO ...
+	// IP-Header checksum
+	binary.BigEndian.PutUint16(packet[10:12], checkSum(packet[:20]))
 
-
+	// TCP-Header
+	
 }
 
 
